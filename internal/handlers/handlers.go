6da5eaf1f489e96a255e20e37023addf4fdfdce6
Salim Bersirov <bersirovsalim@gmail.com>
2026-03-09 19:44:50 +0300

    Complete morse converter implementation

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -1,1 +1,58 @@
-package handlers
+// package handlers
+
+// import (
+// 	"io"
+// 	"net/http"
+// 	"path/filepath"
+// 	"time"
+
+// 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/service"
+// )
+
+// func IndexHandler(w http.ResponseWriter, r *http.Request) {
+//     http.ServeFile(w, r, "./index.html")
+// }
+
+// func UploadHandler(w http.ResponseWriter, r *http.Request) {
+//     if r.Method != http.MethodPost {
+//         http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+//         return
+//     }
+    
+//     err := r.ParseMultipartForm(10 << 20)
+//     if err != nil {
+//         http.Error(w, "Error parsing form", http.StatusInternalServerError)
+//         return
+//     }
+    
+
+//     file, header, err := r.FormFile("myFile")
+//     if err != nil {
+//         http.Error(w, "Error getting file", http.StatusBadRequest)
+//         return
+//     }
+//     defer file.Close()
+    
+//     data, err := io.ReadAll(file)
+//     if err != nil {
+//         http.Error(w, "Error reading file", http.StatusInternalServerError)
+//         return
+//     }
+    
+//     converted, err := service.DetectAndConvert(string(data))
+//     if err != nil {
+//         http.Error(w, "Error converting: " + err.Error(), http.StatusInternalServerError)
+//         return
+//     }
+    
+//     ext := filepath.Ext(header.Filename)
+//     if ext == "" {
+//         ext = ".txt"
+//     }
+    
+//     newFilename := time.Now().UTC().Format("20060102-150405") + ext
+    
+//     w.Header().Set("Content-Disposition", "attachment; filename="+newFilename)
+//     w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+//     w.Write([]byte(converted))
+// }
--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,40 @@
-package server
+// package server
+
+// import (
+// 	"log"
+// 	"net/http"
+// 	"time"
+
+// 	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+// )
+
+// type Server struct {
+//     logger     *log.Logger
+//     httpServer *http.Server
+// }
+
+// func NewServer(logger *log.Logger) *Server {
+//     router := http.NewServeMux()
+    
+//     router.HandleFunc("/", handlers.IndexHandler)
+//     router.HandleFunc("/upload", handlers.UploadHandler)
+    
+//     httpServer := &http.Server{
+//         Addr:         ":8080",
+//         Handler:      router,
+//         ErrorLog:     logger,
+//         ReadTimeout:  5 * time.Second,
+//         WriteTimeout: 10 * time.Second,
+//         IdleTimeout:  15 * time.Second,
+//     }
+    
+//     return &Server{
+//         logger:     logger,
+//         httpServer: httpServer,
+//     }
+// }
+
+// // Run запускает сервер
+// func (s *Server) Run() error {
+//     return s.httpServer.ListenAndServe()
+// }
